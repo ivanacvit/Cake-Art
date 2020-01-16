6babf36c657e43d04c8b38d27f58648b9de48ccb
Ivana Cvitkovic <ivana.cvitkovic118@gmail.com>
2020-01-17 00:35:11 +0100

    comment-style

--- src/pages/comment.js
+++ src/pages/comment.js
@@ -11,59 +11,57 @@ import styles from "../../styles/private.module.css"
 
 const Comment = () => {
   return (
-   <>
-      {isLoggedIn() ? (
-        <Layout>
-        <Container>
-          <h2>COMMENTS</h2>
-         
-          <Comments />
-         
-        </Container>
-        </Layout>
-      ) : (
-        <Layout>
-        <Container>
-          <div
-          sx={{ paddingTop: [5], textAlign: "center"}}>
-            <p
-              sx={{
-                color: "grey",
-                letterSpacing: "tight",
-                paddingBottom: "20px",
-              }}
-            >
-              If you want give us a comment, please click here:
-            </p>
-            <Link
-              to="/"
-              onClick={event => {
-                event.preventDefault()
-
-                navigate("/private")
-              }}
-              className={styles.Logout}
-            >
-               <button
-                sx={{
-                  backgroundColor: "white",
-                  color: "#F78DA7",
-                  border: "white",
-                  cursor: "pointer",
-                  fontSize: "inherit",
-                  outline: "0",
-                }}
-              >
-                 Login
-              </button>
-             
-            </Link>
-          </div>
-        </Container>
-        </Layout>
-      )}
+    <>
+      <div sx={{ paddingTop: [4], textAlign: "center" }}>
+        <p
+          sx={{
+            color: "grey",
+            letterSpacing: "tight",
+            paddingBottom: "20px",
+          }}
+        >
+          If you want give us a comment, please click here:
+        </p>
+        <Link
+          to="/"
+          onClick={event => {
+            event.preventDefault()
+            navigate("/private")
+          }}
+          className={styles.Logout}
+        >
+          <button
+            sx={{
+              backgroundColor: "white",
+              color: "#F78DA7",
+              border: "white",
+              cursor: "pointer",
+              fontSize: "inherit",
+              outline: "0",
+            }}
+          >
+            Login
+          </button>
+        </Link>
+      </div>
+      
     </>
   )
 }
 
-export default Comment
+export default () => (
+  <>
+    <Layout>
+      <Container>
+        <h2>COMMENT</h2>
+        {isLoggedIn() ? (
+          <Comments />
+        ) : (
+          <>
+            <Comment />
+          </>
+        )}
+      </Container>
+    </Layout>
+  </>
+)
