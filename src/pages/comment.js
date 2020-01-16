3aa1e8e2b965c7dacaa36bcb184ca7a7dc056569
Ivana Cvitkovic <ivana.cvitkovic118@gmail.com>
2020-01-17 00:54:46 +0100

    comment-message

--- src/pages/comment.js
+++ src/pages/comment.js
@@ -8,59 +8,25 @@ import Comments from "../components/comments/CommentList"
 import { Link, navigate } from "gatsby"
 import { getUser, isLoggedIn, logout } from "../../services/authentication"
 import styles from "../../styles/private.module.css"
+import CommentMessage from "../components/comments/comment-message"
 
-const Comment = () => (
-  
+const Comment = () => {
+  return (
     <>
-      <div sx={{ paddingTop: [4], textAlign: "center" }}>
-        <p
-          sx={{
-            color: "grey",
-            letterSpacing: "tight",
-            paddingBottom: "20px",
-          }}
-        >
-          If you want give us a comment, please click here:
-        </p>
-        <Link
-          to="/"
-          onClick={event => {
-            event.preventDefault()
-            navigate("/private")
-          }}
-          className={styles.Logout}
-        >
-          <button
-            sx={{
-              backgroundColor: "white",
-              color: "#F78DA7",
-              border: "white",
-              cursor: "pointer",
-              fontSize: "inherit",
-              outline: "0",
-            }}
-          >
-            Login
-          </button>
-        </Link>
-      </div>
-      
+      <Layout>
+        <Container>
+          <h2>COMMENT</h2>
+          {isLoggedIn() ? (
+            <Comments />
+          ) : (
+            <>
+              <CommentMessage />
+            </>
+          )}
+        </Container>
+      </Layout>
     </>
   )
+}
 
-export default () => (
-  <>
-    <Layout>
-      <Container>
-        <h2>COMMENT</h2>
-        {isLoggedIn() ? (
-          <Comments />
-        ) : (
-          <>
-            <Comment />
-          </>
-        )}
-      </Container>
-    </Layout>
-  </>
-)
+export default Comment
